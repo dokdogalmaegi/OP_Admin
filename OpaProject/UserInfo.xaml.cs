af11b03d7c57e66105b0db40420501ec74cdcb0e
임준영 <clik202012@gmail.com>
2021-05-26 22:02:40 +0900

    레이아웃 수정

--- OpaProject/UserInfo.xaml.cs
+++ OpaProject/UserInfo.xaml.cs
@@ -38,9 +38,8 @@ namespace OpaProject
                     break;
                 case Grade.AllGrade :
                     GradeBox.Visibility = Visibility.Hidden; gradeNum.Visibility = Visibility.Hidden; BorderBox.Visibility = Visibility.Hidden; classNumText.Visibility = Visibility.Hidden; classNumNum.Visibility = Visibility.Hidden;
-                    Thickness thickness = schoolIcon.Margin;
-                    thickness.Left = 200;
-                    accountBox.Kind = MaterialDesignThemes.Wpf.PackIconKind.AccountStar; schoolIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.AccountTie; AdminBox.Visibility = Visibility.Visible; LeftBox.Width = new GridLength(3, GridUnitType.Star); schoolIcon.Margin = thickness;
+                    schoolIcon.Visibility = Visibility.Hidden;
+                    AdminBox.Visibility = Visibility.Visible; LeftBox.Width = new GridLength(3, GridUnitType.Star);
                     break;
             }
             switch (classNum)
