c1974bbb7b35b062ab19b3b1b36680ed3e2426aa
임준영 <52159385+dokdogalmaegi@users.noreply.github.com>
2021-06-08 10:28:22 +0900

    더블 클릭 이벤트

--- OpaProject/StudentList.xaml.cs
+++ OpaProject/StudentList.xaml.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace OpaProject
 {
@@ -10,13 +11,34 @@ namespace OpaProject
     /// </summary>
     public partial class StudentList : UserControl
     {
+        private List<deleteStudent> selectedStudent;
         public StudentList(List<Student> studentsTrue, List<Student> studentsFalse)
         {
             InitializeComponent();
+
             if (studentsTrue.Count() > 0) StudentsTrue.ItemsSource = studentsTrue;
             else noticeText2.Visibility = Visibility.Visible;
             if (studentsFalse.Count() > 0) StudentsFalse.ItemsSource = studentsFalse;
             else noticeText.Visibility = Visibility.Visible;
+
+            StudentsTrue.MouseDoubleClick += StudentsTrue_MouseDoubleClick;
+            StudentsFalse.MouseDoubleClick += StudentsFalse_MouseDoubleClick;
+        }
+
+        private void StudentsTrue_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(StudentsTrue.SelectedIndex != -1)
+            {
+                // 아이템 클릭시 할 짓
+                MessageBox.Show("test");
+            }
+        }
+        private void StudentsFalse_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (StudentsTrue.SelectedIndex != -1)
+            {
+                // 아이템 클릭시 할 짓
+            }
         }
     }
 }
